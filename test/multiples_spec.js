ce653d17f97dde1eed512500cef30aae64e37bf6
mellaniesoriano <msoriano808@gmail.com>
2017-05-15 20:43:31 -1000

    fixed last two tests

--- test/multiples_spec.js
+++ test/multiples_spec.js
@@ -16,20 +16,20 @@ describe( 'Sum of a Multiple Function', function() {
 
   // when your tests pass, uncomment the next two and
   // change the expected value to your calculated result
-  /*
-  it( 'should return \'x\' for n=1000 (see multiples-spec.js)', function() {
+
+  it( 'should return \'233168\' for n=1000 (see multiples-spec.js)', function() {
     var sum = multiples.sumOfAMultiple(1000);
 
     // Change the argument given to .equal() to the correct number
-    sum.should.equal(0);
+    sum.should.equal(233168);
   });
 
-  it( 'should return \'x\' for n=10000 (see multiples-spec.js)', function() {
+  it( 'should return \'23331668\' for n=10000 (see multiples-spec.js)', function() {
     var sum = multiples.sumOfAMultiple(10000);
 
     // Change the argument given to .equal() to the correct number
-    sum.should.equal(0);
+    sum.should.equal(23331668);
   });
-  */
+
 
 });
