5b9c6f1c40cace10cc50806413ed0a3c266d6701
mellaniesoriano <msoriano808@gmail.com>
2017-05-15 20:21:34 -1000

    sumOfAMulitple func done

--- multiples.js
+++ multiples.js
@@ -7,6 +7,10 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
-
+  for(var i = 0; i < n; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
   return sum;
 };
