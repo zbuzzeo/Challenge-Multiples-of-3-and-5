3c6e27a6ae06e6b1b881a1181dbcf79e1af4856b
Mellanie Soriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-01-03 09:01:13 -1000

    Update multiples.js

--- multiples.js
+++ multiples.js
@@ -7,10 +7,6 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
-  for(var i = 0; i < n; i++){
-    if(i % 3 === 0 || i % 5 === 0){
-      sum += i;
-    }
-  }
+  
   return sum;
 };
